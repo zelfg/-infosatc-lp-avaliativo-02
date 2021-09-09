0d931bccf8ab0702492567e1ad31b109486ceaf6
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-09 17:20:18 -0300

    Exercício7

---
+++ Ex7.py
@@ -0,0 +1,10 @@
+valor1 = int(input("Digite o primeiro número: "))
+valor2 = int(input("Digite o segundo número: ")) 
+valor3 = int(input("Digite o terceiro número: "))
+valor4 = int(input("Digite o quarto número: "))
+valor5 = int(input("Digite o quinto número: "))
+
+lista = [valor1, valor2, valor3, valor4, valor5]
+
+print(sorted(lista))
+
