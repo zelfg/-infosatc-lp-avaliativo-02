92cdac421794f9eb5c3a1ea634dc44fcf8f7be72
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-09 16:06:00 -0300

    Exercício1

---
+++ Ex1.py
@@ -0,0 +1,9 @@
+item1 = float(input("Digite o primeiro valor: "))
+item2 = float(input("Digite o segundo valor: "))
+item3 = float(input("Digite o terceiro valor: "))
+item4 = float(input("Digite o quarto valor: "))
+item5 = float(input("Digite o quinto valor: "))
+
+lista = [item1, item2, item3, item4, item5]
+
+print(sum(lista))
