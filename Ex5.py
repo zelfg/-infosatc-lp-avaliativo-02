d3e2af5eecf00a53df9d78c3f9afa9ae00d1abd4
Luiz Gustavo <luizmotta0406@gmail.com>
2021-09-09 17:19:58 -0300

    Exercício5

---
+++ Ex5.py
@@ -0,0 +1,33 @@
+ListaA = []
+ListaB = []
+ListaC = []
+ListaD = []
+
+#a.
+ListaA.append("Homem Aranha")    #Filme
+ListaA.append("Taxi Driver")
+
+ListaB.append("Minecraft")       #Jogo
+ListaB.append("Dark Souls")
+
+ListaC.append("Cujo")            #Livro
+ListaC.append("Percy Jackson")
+
+ListaD.append("Volei")           #Esporte
+ListaD.append("Tênis")
+
+#b.
+Lista = ListaA + ListaB + ListaC + ListaD
+
+#c.
+print(ListaC[1])
+
+#d.
+Lista.remove("Tênis")
+
+#e.
+Lista = Lista + []
+
+
+
+print(Lista)
